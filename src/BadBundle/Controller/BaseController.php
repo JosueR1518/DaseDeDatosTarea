950da1b21543ad44f1e78d2571bd4e5dfc13fc87
melvin <melvin@localhost>
2016-05-01 00:15:56 -0600

    prueba plantilla

---
+++ src/BadBundle/Controller/BaseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace BadBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class BaseController extends Controller
+{
+    
+    public function sigperaAction()
+    {
+        return $this->render('BadBundle:Default:blank.html.twig');
+    }
+}
